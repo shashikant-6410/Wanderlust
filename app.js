77ea0964297df196f30f1103ef206cc5d66f2f12
Shashikant Yadav <yadavsky6410@gmail.com>
2025-08-09 02:39:30 +0530

    added cookies

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const {reviewSchema}= require("./schema.js");
 const Review = require("./models/review.js");
 const listings = require("./router/listing.js");
 const reviews = require("./router/review.js")
+const session = require("express-session");
+const { createSecretKey } = require("crypto");
 
 const port = 8080;
 //
@@ -23,6 +25,18 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
+const sessionOptions={
+  secret:"mysecretId",
+  resave: false,
+  saveUninitialized:true,
+  cookie:{
+    expires: Date.now()+ 7 * 24 * 60 * 60 * 1000, //one week time from current time
+    maxAge:7 * 24 * 60 * 60 * 1000 ,
+    httpOnly:true
+  }
+}
+
+app.use(session(sessionOptions));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
